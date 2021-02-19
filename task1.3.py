240d7fa1199a6f3ed182b407001d5cb8f54735c7
CharmaineKhanyi <77413396+CharmaineKhanyi@users.noreply.github.com>
2021-02-19 14:19:49 +0200

    Delete task1.3.py

--- task1.3.py
+++
@@ -1,9 +0,0 @@
-def test_number5(x, y):
-    if x == y or abs(x-y) == 65 or (x+y) == 65:
-        return True
-    else:
-        return False
-
-print(test_number5(30, 35))
-print(test_number5(3, 2))
-print(test_number5(2, 2))
