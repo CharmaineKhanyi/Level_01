57da2f846b1b110a1c713e67ce20e283e91de3fc
CharmaineKhanyi <77413396+CharmaineKhanyi@users.noreply.github.com>
2021-02-19 14:30:16 +0200

    Update task1.7.py

--- task1.7.py
+++ task1.7.py
@@ -1,8 +1,8 @@
-def countList(lst1, lst2): 
-    return [sub[item] for item in range(len(lst2)) 
-                      for sub in [lst1, lst2]] 
+def countList(st1, st2): 
+    return [sub[item] for item in range(len(st2)) 
+                      for sub in [st1, st2]] 
       
 # Driver code 
-lst1 = [11, 22, 33] 
-lst2 = [1, 2, 3] 
-print(countList(lst1, lst2)) 
+ st1 = [11, 22, 33] 
+ st2 = [1, 2, 3] 
+print(countList(st1, st2)) 
