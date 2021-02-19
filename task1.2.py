f297c5e6057eeb4e77fc2135af4e3275bc48bbf6
CharmaineKhanyi <77413396+CharmaineKhanyi@users.noreply.github.com>
2021-02-19 14:03:12 +0200

    Delete task1.2.py

--- task1.2.py
+++
@@ -1,9 +0,0 @@
-def test_number5(x, y):
-    if x == 3 or y == 3 and (x+y) == 3:
-        return True
-    else:
-        return False
-
-print(test_number5(7, 2))
-print(test_number5(3, 2))
-print(test_number5(2, 2))
